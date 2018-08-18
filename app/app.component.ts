cd18a10ef48fb6823a3d97997327f29b62f22f7b
Hamed <hamedsalami@gmail.com>
2018-08-18 21:00:47 +0300

    Lesson

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
+    selector: 'app-root',
+    styleUrls: [
+        'app.component.scss'
+    ],
+    template: `
+    <div>
+        {{ title }}
+
     </div>
-  `
+    <div>
+        <input type='text' [value]='name' />
+    </div>
+    `
 })
 export class AppComponent {
-
+  title: string;
+  name: string = 'Hammed';
+  constructor(){
+      this.title = 'Ultimate Angular';
+  }
 }
