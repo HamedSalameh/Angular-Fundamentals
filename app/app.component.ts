6d5c4c32bd98ac70607798f6f960de9663d886a9
Hamed <hamedsalami@gmail.com>
2018-09-02 20:47:33 +0300

    Until ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+    id: number,
+    fullname: string,
+    checkedin: boolean
+}
+
 @Component({
     selector: 'app-root',
     styleUrls: [
@@ -22,24 +28,64 @@ import { Component } from '@angular/core';
         [(ngModel)]="name"
         >
     </div>
+
+    <template  [ngIf]="name.length > 3">
+        <div>
+            Searching for .. {{ name }}
+        </div>
+    </template>
+
     <div *ngIf='name.length > 2'>
         Searching for ...{{ name }}
     </div>
+
+    <div>
+
+        <h3>Airline Passengers</h3>
+        <ul>
+            <li *ngFor='let Passenger of Passengers;let i = index;'>
+                {{ i+1 }} - {{ Passenger.fullname }}
+            </li>            
+        </ul>
+
+        <h4>With web components </h4>
+            <template ngFor let-passenger let-i="index" [ngForOf]="Passengers">
+                <ul>
+                    <li>
+                        {{ i+1 }} : {{ passenger.fullname }}
+                    </li>
+                </ul>
+            </template>
+    </div>
     `
 })
+
+
 export class AppComponent {
-  title: string;
-  name: string = '';
-  constructor(){
-      this.title = 'Ultimate Angular';
-  }
 
-  handleChange(event: any){
-      this.name  = event;
-  }
+    Passengers: Passenger[] = [{
+        id: 1,
+        fullname: 'Hamed',
+        checkedin: false
+    },
+    {
+        id: 2,
+        fullname: 'Nada',
+        checkedin: false
+    }];
 
-  changeName() {
-      this.name = 'reset';
-  }
+    title: string;
+    name: string = '';
+    constructor() {
+        this.title = 'Ultimate Angular';
+    }
+
+    handleChange(event: any) {
+        this.name = event;
+    }
+
+    changeName() {
+        this.name = 'reset';
+    }
 
 }
