0a6e0852c4e6cc87b0b53782b9b7f1449123a5cd
Hamed <hamedsalami@gmail.com>
2018-09-11 06:39:08 +0300

    until end of forms

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
 
 @Component({
     selector: 'passenger-form',
@@ -12,13 +13,35 @@ export class PassengerFormComponent implements OnInit {
     @Input()
     detail: Passenger;
 
+    @Output()
+    update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
+    baggage: Baggage[] = [{
+        key: 'none',
+        value: 'No baggae'
+    },
+    {
+        key: 'hand-only',
+        value: 'Hand Baggage'
+    },
+    {
+        key: 'hand-hold',
+        value: 'Hand and hold baggae'
+    }]
+
     constructor() { }
 
     ngOnInit() { }
 
-    toggleChecking(checkedin: boolean){
-        if (checkedin){
+    toggleChecking(checkedin: boolean) {
+        if (checkedin) {
             this.detail.checkedInDate = Date.now();
+        }
+    }
+
+    handleSubmit(passenger: Passenger, isValid){
+        if(isValid){
+            this.update.emit(passenger);
         }
     }
 }
--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -1,28 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { PassengerDashboardService} from '../../passenger-dashboard.service';
+import { PassengerDashboardService } from '../../passenger-dashboard.service';
 
-import { Passenger} from '../../models/passenger.interface';
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
     selector: 'passenger-viewer',
     styleUrls: ['passenger-viewer.component.scss'],
     templateUrl: 'passenger-viewer.component.html'
 })
-export class PassengerViewerComponent implements OnInit{
+export class PassengerViewerComponent implements OnInit {
 
     passenger: Passenger;
 
-    constructor(private passengerService: PassengerDashboardService){
+    constructor(private passengerService: PassengerDashboardService) {
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
         this.passengerService
-        .getPassenger(3)
-        .subscribe((data: Passenger) => {
-            this.passenger = data
-        });
-        
+            .getPassenger(3)
+            .subscribe((data: Passenger) => {
+                this.passenger = data
+            });
+
+    }
+
+    onUpdatePassenger(event: Passenger) {
+        console.log('passenger-viewer.onUpdatePassenger')
+        console.log(' > passenger-viewer.onUpdatePassenger : ' + (JSON.stringify(event)));
+        this.passengerService
+            .updatePassenger(event)
+            .subscribe((data: Passenger)=> {
+                this.passenger = Object.assign({}, this.passenger, event);
+            });
     }
 
 }
